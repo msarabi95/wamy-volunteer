5072afefe7fde1beed8d039155e8bb461a491a17
Muhammad Saeed Arabi <msarabi95@gmail.com>
2015-01-16 15:06:16 +0300

    Add links to (not yet implemented) code generation view

--- teams/admin.py
+++ teams/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from teams.models import Team, Event
 
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("name", "team", "admin_links")
+
+
 admin.site.register(Team)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
--- teams/models.py
+++ teams/models.py
@@ -17,6 +17,18 @@ class Event(models.Model):
     team = models.ForeignKey(Team, verbose_name=u"الفريق")
     # allow_multiple = models.BooleanField(default=False)  # allow multiple codes to be submitted per user per event
 
+    def admin_links(self):
+        kw = {"args": (self.id,)}
+        links = [
+            (u"اطلب رموزًا", ""),
+            (u"استعرض رموز هذا النشاط", ""),
+        ]
+        for i, (text, url) in enumerate(links):
+            links[i] = "<a href='%s'>%s</a>" % (url, text)
+        return "<br>".join(links)
+    admin_links.allow_tags = True
+    admin_links.short_description = ""
+
     def __unicode__(self):
         return self.name
 
