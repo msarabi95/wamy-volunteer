9303fd3623ad7325d88d00d2d970e902546cfcfb
Muhammad Saeed Arabi <msarabi95@gmail.com>
2015-01-16 13:14:28 +0300

    Add ``Event`` model Configured admin

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,24 @@
+# coding=utf-8
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(u"الاسم", max_length=128)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = u"فريق"
+        verbose_name_plural = u"الفرق"
+
+
+class Event(models.Model):
+    name = models.CharField(u"الاسم", max_length=128)
+    team = models.ForeignKey(Team, verbose_name=u"الفريق")
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = u"نشاط"
+        verbose_name_plural = u"الأنشطة"
