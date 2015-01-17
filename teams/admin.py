51200b3e6afb6c31b7ba3208850692335f4e58de
Muhammad Saeed Arabi <msarabi95@gmail.com>
2015-01-17 08:53:07 +0300

    Create url and primer for create codes view

--- teams/admin.py
+++ teams/admin.py
@@ -1,9 +1,32 @@
+from django.conf.urls import patterns, url
 from django.contrib import admin
+from django.shortcuts import render
 from teams.models import Team, Event
+
 
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ("name", "team", "admin_links")
+
+    # TODO: create codes view and url
+    def get_urls(self):
+        """
+        Add the entries view to urls.
+        """
+        urls = super(EventAdmin, self).get_urls()
+        extra_urls = patterns("",
+            url("^(?P<event_id>\d+)/codes/$",
+                self.admin_site.admin_view(self.create_codes),
+                name="create_codes"),
+        )
+        return extra_urls + urls
+
+    def create_codes(self, request, event_id):
+        """
+        GET: show the code creation form.
+        POST: create codes according to what's posted.
+        """
+        return render(request, "admin/teams/event/codes.html")
 
 
 admin.site.register(Team)
