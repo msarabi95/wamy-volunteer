d0934186a59aa2a00241da62d35749429d81396c
Muhammad Saeed Arabi <msarabi95@gmail.com>
2015-01-18 11:36:50 +0300

    Random string is now generated for each code when it's initialized

--- codes/models.py
+++ codes/models.py
@@ -1,10 +1,13 @@
 # coding=utf-8
+import random
+import string
 from django.conf import settings
 from django.db import models
 from teams.models import Event
 
 
 CODE_STRING_LENGTH = 16
+CODE_STRING_CHARS = string.ascii_uppercase + string.digits
 
 
 class Category(models.Model):
@@ -22,13 +25,28 @@ class Category(models.Model):
 class Code(models.Model):
     category = models.ForeignKey(Category, verbose_name=u"الفئة")
     event = models.ForeignKey(Event, verbose_name=u"النشاط")
-    string = models.CharField(u"النص", max_length=CODE_STRING_LENGTH)
+    string = models.CharField(u"النص", unique=True, max_length=CODE_STRING_LENGTH)
     date_created = models.DateTimeField(u"تاريخ الإنشاء", auto_now_add=True)
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, verbose_name=u"المستخدم")
     date_redeemed = models.DateTimeField(null=True, blank=True, verbose_name=u"تاريخ الاستخدام")
 
     date_downloaded = models.DateTimeField(null=True, blank=True, verbose_name=u"تاريخ التحميل")
+
+    def __init__(self, *args, **kwargs):
+        """
+        Generate a unique string for the current code.
+        """
+        super(Code, self).__init__(*args, **kwargs)
+
+        # If the code is new (not being loaded from db), then generate a unique string
+        if self.string == "":
+            while True:
+                random_string = ''.join(random.choice(CODE_STRING_CHARS) for idx in range(CODE_STRING_LENGTH))
+                # If the string is unique, then break the loop. Otherwise keep generating random strings.
+                if not self.__class__.objects.filter(string=random_string).exists():
+                    break
+            self.string = random_string
 
     def is_redeemed(self):
         return self.user is not None
