01169654287f8ada083e8fcfd93ca42244a586e8
Muhammad Saeed Arabi <msarabi95@gmail.com>
2015-02-01 20:47:46 +0300

    Credit sum for each user now appears in the `User` admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
+# coding=utf-8
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.db.models import Sum
 from userena.admin import UserenaAdmin
 from accounts.models import UserProfile
 
@@ -10,7 +12,18 @@ class UserProfileInline(admin.StackedInline):
 
 class CustomUserAdmin(UserenaAdmin):
     inlines = (UserProfileInline, )
+    readonly_fields = ('get_credit_sum', )
+    list_display = ('username', 'email', 'first_name', 'last_name',
+                    'is_staff', 'is_active', 'date_joined', 'get_credit_sum')
 
+    def queryset(self, request):
+        qs = super(CustomUserAdmin, self).queryset(request)
+        return qs.annotate(credit_sum=Sum('redeemed_codes__category__credit'))
+
+    def get_credit_sum(self, obj):
+        return obj.redeemed_codes.aggregate(code_sum=Sum("category__credit"))['code_sum']
+    get_credit_sum.short_description = u"مجموع الساعات"
+    get_credit_sum.admin_order_field = 'credit_sum'
 
 admin.site.unregister(UserProfile)
 
