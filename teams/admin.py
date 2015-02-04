d7a5395cc998d866526dfd88ab6fa6d327df39db
Muhammad Saeed Arabi <msarabi95@gmail.com>
2015-02-04 07:53:28 +0300

    Admin now (dynamically) displays average score for each evaluation criterion for each activity

--- teams/admin.py
+++ teams/admin.py
@@ -2,12 +2,12 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin, messages
 from django.core.urlresolvers import reverse
+from django.db.models.aggregates import Avg
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from codes.admin import CodeAdmin
 from codes.forms import CreateCodeForm
 from codes.models import Code, Order
-from teams.models import Team, Event
+from teams.models import Team, Event, EvaluationCriterion, CriterionResponse
 
 
 class OrderTabularInline(admin.TabularInline):
@@ -26,6 +26,50 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ("name", "team", "admin_links")
     search_fields = ("name", "team__name")
     inlines = (OrderTabularInline, )
+
+    def get_evaluation_fields(self):
+        """
+        Return a list of field names for evaluation fields; in the format `criterion_X_avg`, where X
+        is the PK of each evaluation criterion.
+        """
+        eval_fields = []
+        for criterion in EvaluationCriterion.objects.all():
+            eval_fields.append("criterion_%s_avg" % criterion.pk)
+        return tuple(eval_fields)
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Dynamically add a field for each evaluation criterion to display the average score on that criterion.
+        """
+        readonly_fields = super(EventAdmin, self).get_readonly_fields(request, obj)
+        return readonly_fields + self.get_evaluation_fields()
+
+    def get_list_display(self, request):
+        list_display = super(EventAdmin, self).get_list_display(request)
+        joined = list(list_display + self.get_evaluation_fields())
+
+        # To make things neater, move `admin_links` field to the end
+        joined.append(joined.pop(joined.index("admin_links")))
+        return tuple(joined)
+
+    def __getattr__(self, item):
+        """
+        For evaluation fields, which are dynamically added, return a callable that returns the average
+         responses for a certain criterion in a certain event.
+        """
+        if item in self.get_evaluation_fields():
+            def get_criterion(str):
+                return EvaluationCriterion.objects.get(pk=int(str.split("_")[1]))
+
+            def func(obj):
+                return CriterionResponse.objects.filter(evaluation__event=obj,
+                                                        criterion=get_criterion(item))\
+                                                        .aggregate(avg=Avg("response"))["avg"]
+            func.short_description = u"معدل تقييم %s" % get_criterion(item).label
+            return func
+
+        else:
+            raise AttributeError
 
     def get_urls(self):
         """
@@ -66,3 +110,4 @@ class EventAdmin(admin.ModelAdmin):
 
 admin.site.register(Team)
 admin.site.register(Event, EventAdmin)
+admin.site.register(EvaluationCriterion)
