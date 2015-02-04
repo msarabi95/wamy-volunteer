65376b8de7831b91b0c0de650de373e3772b12c2
Muhammad Saeed Arabi <msarabi95@gmail.com>
2015-02-04 07:52:44 +0300

    Evaluation models

--- teams/models.py
+++ teams/models.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.db import models
 
@@ -46,3 +47,43 @@ class Event(models.Model):
     class Meta:
         verbose_name = u"نشاط"
         verbose_name_plural = u"الأنشطة"
+
+
+class EvaluationCriterion(models.Model):
+    label = models.CharField(max_length=128, verbose_name=u"العنوان")
+    description = models.CharField(max_length=128, verbose_name=u"الوصف",
+                                   help_text=u"السؤال الذي سيظهر لمن سيقوم بالتقييم.")
+    max_score = models.PositiveIntegerField(default=5, verbose_name=u"الحد الأقصى",
+                                        help_text=u"معيار التقييم عبارة عن سلم من ١ إلى الحد الأقصى الذي يتم تحديده.")
+
+    def __unicode__(self):
+        return self.label
+
+    class Meta:
+        verbose_name = u"معيار تقييم"
+        verbose_name_plural = u"معايير التقييم"
+
+
+class Evaluation(models.Model):
+    event = models.ForeignKey(Event, related_name="evaluations", verbose_name=u"النشاط")
+    user = models.ForeignKey(User, related_name="evaluations", verbose_name=u"المستخدم")
+
+    def __unicode__(self):
+        return "Evaluation of %s by %s" % (self.event.__unicode__(), self.user.__unicode__())
+
+    class Meta:
+        verbose_name = u"تقييم"
+        verbose_name_plural = u"التقييمات"
+
+
+class CriterionResponse(models.Model):
+    evaluation = models.ForeignKey(Evaluation, related_name="criterion_responses")
+    criterion = models.ForeignKey(EvaluationCriterion, verbose_name=u"المعيار")
+    response = models.PositiveIntegerField(verbose_name=u"التقييم")
+
+    def __unicode__(self):
+        return "Response to %s in %s" % (self.criterion.__unicode__(), self.evaluation.__unicode__())
+
+    class Meta:
+        verbose_name = u"تقييم فرعي"
+        verbose_name_plural = u"التقييمات الفرعية"
